98c8c51ef3e35ae5fd974b8d1d7fa8c9804698d4
Evans Rutto <robbyevans001@gmail.com>
2022-06-05 23:13:20 +0300

    commit tree 8aa4aca2044dfde37af99caefa5a940fdeb9abea

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code he
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
